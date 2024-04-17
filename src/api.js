33336ccf96e9e95fd65e51b7aef5d263beb340cf
Thomas Kim <tkimm156@gmail.com>
2024-04-17 16:37:35 -0500

    work on exif data

--- src/api.js
+++ src/api.js
@@ -1,10 +1,15 @@
 const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5001/';
+import cities from "cities";
 
 class PixlyAPI {
   static async uploadImage(image, tags) {
     const formData = new FormData();
     formData.append('image', image);
-    console.log(image);
+    const exif = tags;
+    console.log(exif.GPSLongitude.description);
+    formData.append("exif", JSON.stringify(exif));
+    const state = (cities.gps_lookup(exif.GPSLatitude.description, -88)).state;
+    console.log(state);
     const resp = await fetch(`${BASE_URL}images`, {
       method: 'POST',
       mode: 'no-cors',
